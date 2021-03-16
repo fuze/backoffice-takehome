31bc1e2967c6becf9c92564eeb547e72084e8ab8
Nikitha Bangera <nikitha.bangera64@gmail.com>
2021-03-16 18:57:09 -0400

    Task 2 - fixed the broken test case and added some more unit test cases

--- src/test/java/com/fuze/takehome/test/UserTest.java
+++ src/test/java/com/fuze/takehome/test/UserTest.java
@@ -3,9 +3,13 @@ package com.fuze.takehome.test;
 import java.util.List;
 
 import javax.inject.Inject;
+import javax.ws.rs.NotFoundException;
 
 import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import com.fuze.takehome.model.User;
 import com.fuze.takehome.service.UserService;
@@ -14,6 +18,8 @@ public class UserTest extends AbstractEntityTest {
 
 	@Inject
 	private UserService service;
+	@Rule
+	public ExpectedException exceptionRule = ExpectedException.none();
 	
 	/**
 	 * Test the User Entity and User Service.
@@ -26,26 +32,63 @@ public class UserTest extends AbstractEntityTest {
 	 * invoked to insert a couple dummy entities into the database. 
 	 * 
 	 */
+	
+	User newUser = null;
+	User userAddedToDb = null;
+	
+	@Before
+	public void init() {
+		newUser = new User()
+				.withActive(true)
+				.withCustomerId(0L)
+				.withEmail("myTest@test.com")
+				.withFirstName("My")
+				.withLastName("Test")
+				.withMobileNumber("555-2356-254")
+				.withTelephoneNumber("555-8512-763")
+				.withUserName("rcastorena")
+				.withDepartmentId(1L);
+		userAddedToDb = service.create(newUser);
+	}
+	
 	@Test
 	public void testUser() {
 		List<User> allUsers = service.list();
 		Assert.assertNotNull(allUsers);
-		Assert.assertEquals(2, allUsers.size());
+		Assert.assertEquals(3, allUsers.size());
 		
-		User newUser = new User()
-		.withActive(true)
-		.withCustomerId(0L)
-		.withEmail("myTest@test.com")
-		.withFirstName("My")
-		.withLastName("Test")
-		.withMobileNumber("555-2356-254")
-		.withTelephoneNumber("555-8512-763")
-		.withUserName("rcastorena");
-		
-		service.create(newUser);
-		
-		Assert.assertNotNull(newUser.getDepartmentId());
-		
+		Assert.assertNotNull(userAddedToDb.getDepartmentId());
 		//ugghh.... can't be bothered
+		Assert.assertTrue(newUser.equals(userAddedToDb));
 	}
+	
+	@Test
+	public void testReadUser() {
+		User userFromDb = service.read(userAddedToDb.getId());
+		Assert.assertTrue(newUser.getDepartmentId().equals(userFromDb.getDepartmentId()));
+		Assert.assertTrue(newUser.getEmail().toString().equals(userFromDb.getEmail().toString()));
+		Assert.assertTrue(compareTwoObjects(newUser, userFromDb));
+	}
+
+
+	private boolean compareTwoObjects(User newUser2, User userFromDb) {
+		return newUser2.isActive() == userFromDb.isActive() 
+				&& newUser2.getFirstName().toString().equals(userFromDb.getFirstName())
+				&& newUser2.getLastName().toString().equals(userFromDb.getLastName())
+					&& newUser2.getId().equals(userFromDb.getId())
+					 && newUser2.getCustomerId().equals(userFromDb.getCustomerId())
+					 	&& newUser2.getUserName().toString().equals(userFromDb.getUserName().toString());
+	}
+	
+	@Test
+	public void testDeleteUser() {
+		
+		User userFromDb = service.read(userAddedToDb.getId());
+		User deletedUser = service.delete(userFromDb.getId());
+		Assert.assertTrue(compareTwoObjects(newUser, deletedUser));
+		exceptionRule.expect(NotFoundException.class);
+		service.delete(userFromDb.getId());
+	}
+	
+	
 }
