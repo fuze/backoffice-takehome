57f6a22c5e43a47dff4196111b98d4fb1bfd30cd
beau0781 <beau0781@algonquinlive.com>
2021-03-27 22:14:16 -0400

    Exercise 2

--- src/test/java/com/fuze/takehome/test/UserTest.java
+++ src/test/java/com/fuze/takehome/test/UserTest.java
@@ -3,6 +3,7 @@ package com.fuze.takehome.test;
 import java.util.List;
 
 import javax.inject.Inject;
+import javax.ws.rs.NotFoundException;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -14,38 +15,52 @@ public class UserTest extends AbstractEntityTest {
 
 	@Inject
 	private UserService service;
-	
+
 	/**
 	 * Test the User Entity and User Service.
 	 * 
-	 * Assert that entity validation is working correctly and
-	 * the service methods are doing what they are supposed to be doing.
+	 * Assert that entity validation is working correctly and the service methods are doing what they are supposed to be doing.
 	 * 
-	 * Before this test executes, AbstractEntityTest takes care of initializing
-	 * Spring and starting an in-memory DB instance. DummyDataGenerator is
-	 * invoked to insert a couple dummy entities into the database. 
+	 * Before this test executes, AbstractEntityTest takes care of initializing Spring and starting an in-memory DB instance. DummyDataGenerator is invoked to insert a couple dummy entities into the
+	 * database.
 	 * 
 	 */
+
 	@Test
 	public void testUser() {
 		List<User> allUsers = service.list();
 		Assert.assertNotNull(allUsers);
 		Assert.assertEquals(2, allUsers.size());
-		
+
+		// Test create user
 		User newUser = new User()
-		.withActive(true)
-		.withCustomerId(0L)
-		.withEmail("myTest@test.com")
-		.withFirstName("My")
-		.withLastName("Test")
-		.withMobileNumber("555-2356-254")
-		.withTelephoneNumber("555-8512-763")
-		.withUserName("rcastorena");
-		
+				.withActive(true)
+				.withCustomerId(0L)
+				.withDepartmentId(0L)
+				.withEmail("myTest@test.com")
+				.withFirstName("My")
+				.withLastName("Test")
+				.withMobileNumber("555-2356-254")
+				.withTelephoneNumber("555-8512-763")
+				.withUserName("rcastorena");
+
 		service.create(newUser);
-		
-		Assert.assertNotNull(newUser.getDepartmentId());
-		
-		//ugghh.... can't be bothered
+		Assert.assertNotNull(newUser.getId());
+
+		allUsers = service.list();
+		Assert.assertEquals(3, allUsers.size());
+
+		// Test read user from dummy data
+		User testReadUser = service.read(0L);
+		Assert.assertNotNull(testReadUser.getCustomerId());
+
+		// Test delete user from dummy data
+		service.delete(testReadUser.getId());
+
+		exceptionRule.expect(NotFoundException.class);
+		service.read(testReadUser.getId());
+
+		allUsers = service.list();
+		Assert.assertEquals(2, allUsers.size());
 	}
 }
